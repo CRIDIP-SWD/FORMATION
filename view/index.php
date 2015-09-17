7e58c856ac55640d6b23004f28de33a368b7b5d0
CRIDIP-SWD <mmockelyn@cridip.com>
2015-09-17 16:34:47 +0200

    Test

--- view/index.php
+++ view/index.php
@@ -63,27 +63,27 @@ include ('include/header.php');
         <!-- END MESSAGE BOX-->
 
         <!-- START PRELOADS -->
-        <audio id="audio-alert" src="audio/alert.mp3" preload="auto"></audio>
-        <audio id="audio-fail" src="audio/fail.mp3" preload="auto"></audio>
+        <audio id="audio-alert" src="<?= ROOT,ASSETS,AUDIO; ?>alert.mp3" preload="auto"></audio>
+        <audio id="audio-fail" src="<?= ROOT,ASSETS,AUDIO; ?>fail.mp3" preload="auto"></audio>
         <!-- END PRELOADS -->               
 
     <!-- START SCRIPTS -->
         <!-- START PLUGINS -->
-        <script type="text/javascript" src="js/plugins/jquery/jquery.min.js"></script>
-        <script type="text/javascript" src="js/plugins/jquery/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="js/plugins/bootstrap/bootstrap.min.js"></script>        
+        <script type="text/javascript" src="<?= ROOT,ASSETS,JS,PLUGINS; ?>jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="<?= ROOT,ASSETS,JS,PLUGINS; ?>jquery/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="<?= ROOT,ASSETS,JS,PLUGINS; ?>bootstrap/bootstrap.min.js"></script>
         <!-- END PLUGINS -->
 
         <!-- THIS PAGE PLUGINS -->
-        <script type='text/javascript' src='js/plugins/icheck/icheck.min.js'></script>
-        <script type="text/javascript" src="js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js"></script>
+        <script type='text/javascript' src='<?= ROOT,ASSETS,JS,PLUGINS; ?>icheck/icheck.min.js'></script>
+        <script type="text/javascript" src="<?= ROOT,ASSETS,JS,PLUGINS; ?>mcustomscrollbar/jquery.mCustomScrollbar.min.js"></script>
         <!-- END PAGE PLUGINS -->       
 
         <!-- START TEMPLATE -->
-        <script type="text/javascript" src="js/settings.js"></script>
+        <script type="text/javascript" src="<?= ROOT,ASSETS,JS; ?>settings.js"></script>
         
-        <script type="text/javascript" src="js/plugins.js"></script>        
-        <script type="text/javascript" src="js/actions.js"></script>        
+        <script type="text/javascript" src="<?= ROOT,ASSETS,JS; ?>plugins.js"></script>
+        <script type="text/javascript" src="<?= ROOT,ASSETS,JS; ?>actions.js"></script>
         <!-- END TEMPLATE -->
     <!-- END SCRIPTS -->         
     </body>
