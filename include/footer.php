28aa2b5d374148fb40649951f26c2099fb7a5688
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-20 18:45:56 +0200

    FORMATION

---
+++ include/footer.php
@@ -0,0 +1,29 @@
+<!-- BEGIN FOOTER -->
+<div class="page-footer">
+    <div class="page-footer-inner"> 2015 &copy; CRIDIP-SWD: NUM IND12209LOP.</div>
+    <div class="scroll-to-top">
+        <i class="icon-arrow-up"></i>
+    </div>
+</div>
+<!-- END FOOTER -->
+<!--[if lt IE 9]>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>respond.min.js"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>excanvas.min.js"></script>
+<![endif]-->
+<!-- BEGIN CORE PLUGINS -->
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>jquery.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>jquery.blockui.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>uniform/jquery.uniform.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<script src="<?= ROOT,ASSETS,JS; ?>app.min.js" type="text/javascript"></script>
+<!-- END THEME GLOBAL SCRIPTS -->
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<script src="<?= ROOT,ASSETS,JS; ?>layout.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,JS; ?>demo.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,JS; ?>quick-sidebar.min.js" type="text/javascript"></script>
+<!-- END THEME LAYOUT SCRIPTS -->
