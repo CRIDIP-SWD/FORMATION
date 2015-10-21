de7e052b85dead1ccbe838810f1874e39217fb94
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-21 18:48:47 +0200

    FORMATION

--- include/sidebar.php
+++ include/sidebar.php
@@ -11,20 +11,44 @@
         <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
         <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
         <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
-        <ul class="page-sidebar-menu   " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
-            <li class="nav-item <?php if($_GET['sub'] == 'info'){echo "active open";} ?>">
-                <a href="index.php?view=index&sub=info" class="nav-link nav-toggle">
-                    <i class="icon-user"></i>
-                    <span class="title">Vos Informations</span>
-                </a>
-            </li>
-            <li class="nav-item <?php if($_GET['sub'] == 'formation'){echo "active open";} ?>">
-                <a href="index.php?view=index&sub=formation" class="nav-link nav-toggle">
-                    <i class="icon-calendar"></i>
-                    <span class="title">Vos Formations</span>
-                </a>
-            </li>
-        </ul>
+        <?php if($info_user['type'] == 0){ ?>
+            <ul class="page-sidebar-menu   " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+                <li class="nav-item <?php if($_GET['view'] == 'client'){echo "active open";} ?>">
+                    <a href="index.php?view=client" class="nav-link nav-toggle">
+                        <i class="icon-user"></i>
+                        <span class="title">Client</span>
+                    </a>
+                </li>
+                <li class="nav-item <?php if($_GET['view'] == 'demande'){echo "active open";} ?>">
+                    <a href="index.php?view=demande" class="nav-link nav-toggle">
+                        <i class="icon-file"></i>
+                        <span class="title">Demande</span>
+                    </a>
+                </li>
+                <li class="nav-item <?php if($_GET['view'] == 'calendar'){echo "active open";} ?>">
+                    <a href="index.php?view=calendar" class="nav-link nav-toggle">
+                        <i class="icon-calendar"></i>
+                        <span class="title">Formation</span>
+                    </a>
+                </li>
+            </ul>
+        <?php } ?>
+        <?php if($info_user['type'] == 1){ ?>
+            <ul class="page-sidebar-menu   " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+                <li class="nav-item <?php if($_GET['view'] == 'info'){echo "active open";} ?>">
+                    <a href="index.php?view=info" class="nav-link nav-toggle">
+                        <i class="icon-user"></i>
+                        <span class="title">Vos Informations</span>
+                    </a>
+                </li>
+                <li class="nav-item <?php if($_GET['view'] == 'formation'){echo "active open";} ?>">
+                    <a href="index.php?view=formation" class="nav-link nav-toggle">
+                        <i class="icon-calendar"></i>
+                        <span class="title">Vos Formations</span>
+                    </a>
+                </li>
+            </ul>
+        <?php } ?>
         <!-- END SIDEBAR MENU -->
     </div>
     <!-- END SIDEBAR -->
--- index.php
+++ index.php
@@ -11,6 +11,26 @@
                 include ('view/login.php');
                 break;
 
+            case "client":
+                include "view/client.php";
+                break;
+
+            case "demande":
+                include "view/demande.php";
+                break;
+
+            case "calendar":
+                include "view/calendar.php";
+                break;
+
+            case "info":
+                include "view/info.php";
+                break;
+
+            case "formation":
+                include "view/formation.php";
+                break;
+
         }
     }else{
         include ('view/error-no-redirect.php');
