ddbd7c5a98c2f1507e1c06c1cae55cfe493ade51
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-21 10:56:13 +0200

    FORMATION

---
+++ view/login.php
@@ -0,0 +1,111 @@
+<?php include "include/config.php"; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+
+<head>
+    <meta charset="utf-8" />
+    <title><?= NOM_LOGICIEL; ?> - Connexion</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+    <link href="<?= ROOT,ASSETS,PLUGINS; ?>plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= ROOT,ASSETS,PLUGINS; ?>plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= ROOT,ASSETS,PLUGINS; ?>plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= ROOT,ASSETS,PLUGINS; ?>plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css" />
+    <link href="<?= ROOT,ASSETS,PLUGINS; ?>plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN PAGE LEVEL PLUGINS -->
+    <link href="<?= ROOT,ASSETS,PLUGINS; ?>plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= ROOT,ASSETS,PLUGINS; ?>plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL PLUGINS -->
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="<?= ROOT,ASSETS,CSS; ?>components-rounded.min.css" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="<?= ROOT,ASSETS,CSS; ?>plugins.min.css" rel="stylesheet" type="text/css" />
+    <!-- END THEME GLOBAL STYLES -->
+    <!-- BEGIN PAGE LEVEL STYLES -->
+    <link href="<?= ROOT,ASSETS,CSS; ?>login-5.min.css" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL STYLES -->
+    <!-- BEGIN THEME LAYOUT STYLES -->
+    <!-- END THEME LAYOUT STYLES -->
+    </head>
+<!-- END HEAD -->
+
+<body class=" login">
+<!-- BEGIN : LOGIN PAGE 5-1 -->
+<div class="user-login-5">
+    <div class="row bs-reset">
+        <div class="col-md-6 bs-reset">
+            <div class="login-bg" style="background-image:url(<?= ROOT,ASSETS,IMG; ?>login/bg1.jpg)">
+                <img class="login-logo" src="<?= ROOT,ASSETS,IMG; ?>login/logo.png" /> </div>
+        </div>
+        <div class="col-md-6 login-container bs-reset">
+            <div class="login-content">
+                <h1><?= NOM_LOGICIEL; ?></h1>
+                <p> Veuillez rentrer vos identifiants afin d'accéder à votre interface ! </p>
+                <form action="javascript:;" class="login-form">
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <input type="text" placeholder="Nom d'utilisateur" class="form-control login-username" id="login-username" name="login" /> </div>
+                        <div class="col-xs-6">
+                            <input type="password" placeholder="Mot de Passe" class="form-control login-password" id="login-password" name="password" /> </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+                        </div>
+                        <div class="col-sm-8 text-right">
+                            <button class="btn blue" type="submit" name="action" value="Connexion">Connexion</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="login-footer">
+                <div class="row bs-reset">
+                    <div class="col-xs-4 bs-reset">
+
+                    </div>
+                    <div class="col-xs-8 bs-reset">
+                        <div class="login-copyright text-right">
+                            <p>Copyright 2015 &copy; CRIDIP-SWD: NUM IND12209LOP.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END : LOGIN PAGE 5-1 -->
+<!--[if lt IE 9]>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>respond.min.js"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>excanvas.min.js"></script>
+<![endif]-->
+<!-- BEGIN CORE PLUGINS -->
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>jquery.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>jquery.blockui.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>uniform/jquery.uniform.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>select2/js/select2.full.min.js" type="text/javascript"></script>
+<script src="<?= ROOT,ASSETS,PLUGINS; ?>backstretch/jquery.backstretch.min.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<script src="<?= ROOT,ASSETS,JS; ?>app.min.js" type="text/javascript"></script>
+<!-- END THEME GLOBAL SCRIPTS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="<?= ROOT,ASSETS,JS; ?>login-5.min.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL SCRIPTS -->
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<!-- END THEME LAYOUT SCRIPTS -->
+</body>
+
+</html>
