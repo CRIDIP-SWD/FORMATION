49a479b28917c1d897812f3cdef63da00eaa7655
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-22 19:56:25 +0200

    FORMATION

--- control/commande.php
+++ control/commande.php
@@ -141,6 +141,86 @@ if(isset($_POST['action']) && $_POST['action'] == 'add-formation-catalogue')
         mysql_query("INSERT INTO `ligne_commande_catalogue`(`idligne`, `idcommande`, `idproduit`) VALUES (NULL,'$idcommande','$valeur')")or die(mysql_error());
     }
 
+    //Mail
+    $to = $mail .',';
+    $to .= 'mmockelyn@cridip.com';
+
+    $sujet = "<".NOM_LOGICIEL."> Nouvelle Demande de formation INTER ENTREPRISE";
+
+    ob_start();
+    ?>
+    <html>
+    <head>
+
+    </head>
+    <body>
+    <table style="width: 100%;">
+        <tr>
+            <td style="text-align: center; vertical-align: middle; font-weight: bold;"><?= NOM_LOGICIEL; ?></td>
+        </tr>
+    </table>
+    <table style="width: 100%;">
+        <tr>
+            <td style="text-align: left;"><strong>Objet:</strong> Nouvelle Demande de formation INTER ENTREPRISE</td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td>
+                Bonjour,<br>
+                <br>
+                Enregistrement de la demande:<br />
+                <table style="width: 100%; border: solid 2px; border-radius: 5px;">
+                    <tr>
+                        <td style="width: 50%;border-bottom: solid 1px; border-right: solid 1px; font-weight: bold;">Client Concerné:</td>
+                        <td style="width: 50%;border-bottom: solid 1px;">
+                            <strong><?= $client['nom_societe']; ?></strong><br>
+                            <i><?= $contact['nom_contact']; ?> <?= $contact['prenom_contact']; ?></i><br>
+                            <strong>Tel:</strong> <?= $contact['tel_contact']; ?><br>
+                            <strong>Port:</strong> <?= $contact['port_contact']; ?><br>
+                            <strong>Mail:</strong> <?= $contact['mail_contact']; ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 50%;border-bottom: solid 1px; border-right: solid 1px; font-weight: bold;">Liste des Formations Concernées:</td>
+                        <td style="width: 50%;border-bottom: solid 1px;">
+                            <?php
+                            $sql_ligne = mysql_query("SELECT * FROM ligne_commande_catalogue, catalogue WHERE ligne_commande_catalogue.idproduit = catalogue.idproduit AND idcommande = '$idcommande'")or die(mysql_error());
+                            while($ligne = mysql_fetch_array($sql_ligne))
+                            {
+                                echo "(Réf.".$ligne['ref_produit'].") ".$ligne['designation_produit'];
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 50%;border-bottom: solid 1px; border-right: solid 1px; font-weight: bold;">Date Choisie:</td>
+                        <td style="width: 50%;border-bottom: solid 1px;"><?= $date_class->jour_semaine(date('N', $date_formation)); ?> <?= date('d', $date_formation); ?> <?= $date_class->mois(date('n', $date_formation)); ?> <?= date('Y', $date_formation); ?></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 50%;border-bottom: solid 1px; border-right: solid 1px; font-weight: bold;">Nombre de Personne:</td>
+                        <td style="width: 50%;border-bottom: solid 1px;"><?= $nb_personne; ?> Personnes</td>
+                    </tr>
+                </table><br><br>
+                Nous restons à votre disposition pour toutes informations complémentaires.<br><br>
+                Cordialement,<br>
+                <br>
+                Support Technique<br>
+                <i><?= NOM_LOGICIEL; ?></i>
+            </td>
+        </tr>
+    </table>
+    </body>
+    </html>
+    <?php
+    $message = ob_get_contents();
+
+    $headers = "MIME-Version: 1.0"."\r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8"."\r\n";
+    $headers .= "To: no-reply <no-reply@clh>"."\r\n";
+
+    $mail = mail($to, $sujet, $message, $headers);
+
     if($sql_create_commande === TRUE)
     {
         header("Location: ../index.php?view=index&success=add-formation-catalogue");
